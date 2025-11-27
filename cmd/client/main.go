97754ac735732d43634a41d412d01dac13e1a2aa
Marcio Flavio <mflaviof1995@gmail.com>
2025-11-27 16:01:52 -0300

    feat(client): replace signal wait with interactive command loop
    Refactors the client main function to remove the Ctrl+C signal wait and instead introduce an interactive command loop. Users can now enter commands such as move, spawn, status, help, spam, and quit to interact with the game logic directly from the terminal. This change improves usability by allowing real-time command input and feedback.

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
 
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
@@ -40,9 +38,38 @@ func main() {
 	}
 	fmt.Printf("Queue %v declared and bound!\n", queue.Name)
 
-	// wait for ctrl+c
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
-	fmt.Println("RabbitMQ connection closed.")
+	gs := gamelogic.NewGameState(username)
+
+	for {
+		userInput := gamelogic.GetInput()
+		if len(userInput) == 0 {
+			continue
+		}
+		switch userInput[0] {
+		case "move":
+			_, err := gs.CommandMove(userInput)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+
+		case "spawn":
+			err = gs.CommandSpawn(userInput)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+		case "status":
+			gs.CommandStatus()
+		case "help":
+			gamelogic.PrintClientHelp()
+		case "spam":
+			fmt.Println("Spamming not allowed yet!")
+		case "quit":
+			gamelogic.PrintQuit()
+			return
+		default:
+			fmt.Println("unknown command")
+		}
+	}
 }
